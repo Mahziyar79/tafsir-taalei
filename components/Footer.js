ad6a6ef8768efdf7832db5a9ca004c34e5169b00
Mahziyar Gilanpour <mgillanpour79@gmail.com>
2024-03-31 18:20:51 +0330

    change some styles

--- components/Footer.js
+++ components/Footer.js
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col md:flex-row gap-4 items-center justify-between mt-20 pt-8 pb-4 border-t-2 border-gray-200">
+    <footer className="flex justify-center mt-20 pt-8 pb-4 border-t-2 border-gray-200">
       <div className="flex items-center gap-7">
         <Link href={"/terms-and-conditions"}>Terms and Conditions</Link>
         <Link href={"/privacy-policy"}>Privacy Policy</Link>
       </div>
-      <p>Copyright ©2024 forosha.ir</p>
+      
     </footer>
   );
 };
--- components/HomeBanner.js
+++ components/HomeBanner.js
@@ -5,8 +5,8 @@ import Link from "next/link";
 const HomeBanner = () => {
   return (
     <section className="bg-[#1E3A8A] p-10 rounded-[20px]">
-      <div className="flex flex-col items-center justify-center py-[30px] px-[40px] rounded-[20px] bg-cover bg-[url('../public/assets/images/bg.png')]">
-        <h1 className="text-3xl md:text-6xl mb-12 text-white font-bold">Quran Hadi</h1>
+      <div className="flex flex-col items-center justify-center py-[20px] px-[30px md:py-[30px] md:px-[40px] rounded-[20px] bg-cover bg-[url('../public/assets/images/bg.png')]">
+        <h1 className="text-4xl md:text-6xl mb-12 text-white font-bold">Quran Hadi</h1>
         <p className="mb-6 text-white text-center">
           With English Translation and Interpretation (Tafsir) (Ahlul‑Bayt)
         </p>
@@ -14,7 +14,7 @@ const HomeBanner = () => {
           <Image
             src={appleStore}
             alt="apple store logo"
-            width={262}
+            width={230}
             height={78}
           />
         </Link>
