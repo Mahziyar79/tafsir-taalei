804ef7481d9bf68ed691026fdd3fc3973c5ed28d
Mahziyar Gilanpour <mgillanpour79@gmail.com>
2024-03-31 20:40:37 +0330

    update logo

--- components/HomeBanner.js
+++ components/HomeBanner.js
@@ -6,9 +6,11 @@ const HomeBanner = () => {
   return (
     <section className="bg-[#1E3A8A] p-10 rounded-[20px]">
       <div className="flex flex-col items-center justify-center py-[20px] px-[30px md:py-[30px] md:px-[40px] rounded-[20px] bg-cover bg-[url('../public/assets/images/bg.png')]">
-        <h1 className="text-4xl md:text-6xl mb-12 text-white font-bold">Quran Hadi</h1>
+        <h1 className="text-4xl md:text-6xl mb-12 text-white font-bold">
+          Quran Tafsir
+        </h1>
         <p className="mb-6 text-white text-center">
-          With English Translation and Interpretation (Tafsir) (Ahlul‑Bayt)
+          with Translation and Interpretationtation
         </p>
         <Link href={"https://www.apple.com/uk/app-store/"}>
           <Image
